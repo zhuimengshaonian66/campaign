cb68a15a36cee89a197296e694ce7a9783475605
少年追梦 <zhuimengshaonian04@gmail.com>
2018-07-22 10:48:59 +0800

    part_2

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-part_1
+
+part_2
+
+1、添加card  raect UI，注意map的使用
+<Card.Group items={items}/>
+
+2、由于next不支持css模块，只能通过link的方式手动添加
+  <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.1/semantic.min.css"></link>
+
+
+3、
+return{
+          header:address,
+          description:<a>查看众筹</a>,
+          fluid:true
+      }
+
+写为 会报错
+return
+{
+          header:address,
+          description:<a>查看众筹</a>,
+          fluid:true
+      }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import factory from '../ethereum/factory';
+
+import {Card} from 'semantic-ui-react';
 // export default ()=>{
 //
 // return <h1>this is new page</h1>;
@@ -9,23 +11,34 @@ import factory from '../ethereum/factory';
 class CompaignIndex extends  React.Component{
 
     static async getInitialProps(){
-        const campaigns = await factory.methods.getDeployedCampain().call();
-        return {campaigns};
+          const campaigns = await factory.methods.getDeployedCampain().call();
+          return {campaigns};
     }
 
-
-    // async componentDidMount(){
     //
+    // async componentDidMount(){
     //   const campaign = await factory.methods.getDeployedCampain().call();
     //   console.log(campaign);
     // }
 
 
+renderCampains(){
+  const items = this.props.campaigns.map(address=>{
+      return{
+          header:address,
+          description:<a>查看众筹</a>,
+          fluid:true
+      }
+  });
+return <Card.Group items={items}/>;
 
+}
 
     render(){
-
-      return <div>{this.props.campaigns[0]}</div>;
+      return <div>
+      <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.1/semantic.min.css"></link>
+      {this.renderCampains()}
+      </div>;
     }
 }
 
